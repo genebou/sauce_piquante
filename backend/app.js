0b9f3348bc71f16118ae4f1fd89b282e4000ea8d
Geneviève B <genevieve.bourchis@gmail.com>
2023-03-07 18:44:26 +0100

    mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,11 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+mongoose.connect('mongodb+srv://genevievebourchis@gmail.com:a123456bh@cluster0.nhotc3b.mongodb.net/test?retryWrites=true&w=majority', 
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 app.use((req, res, next) => {
     console.log('Requête reçue !');
     next();
