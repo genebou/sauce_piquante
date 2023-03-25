916ab91a8b85b6bcbe47557382ad803cc249f2b1
Geneviève B <genevieve.bourchis@gmail.com>
2023-03-25 10:50:17 +0100

    delete, like et dislike

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -15,7 +15,10 @@ const auth = require('../middleware/auth');
 //--Create Sauce-- 
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.get('/',auth,sauceCtrl.getAllSauces);
-
+router.get('/:id',auth,sauceCtrl.getOneSauce);
+router.put('/:id',auth,multer,sauceCtrl.modifySauce);
+router.delete('/:id',auth,sauceCtrl.deleteSauce);
+router.post('/:id/like',auth,sauceCtrl.likeSauce);
 
     
 module.exports = router;
